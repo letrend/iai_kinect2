a1d06467541ebeddf8935d0f9ced4a5607e47835
letrend <simontrendel@gmail.com>
2021-05-17 11:08:04 +0200

    need this for opencv>=4 aswell

--- kinect2_calibration/src/kinect2_calibration.cpp
+++ kinect2_calibration/src/kinect2_calibration.cpp
@@ -1091,7 +1091,7 @@ void getPlane(const size_t index, cv::Mat &normal, double &distance) const
         //cv::solvePnP(board, points[index], cameraMatrix, distortion, rvec, translation, false, cv::EPNP);
 #if CV_MAJOR_VERSION == 2
         cv::solvePnPRansac(board, points[index], cameraMatrix, distortion, rvec, translation, false, 300, 0.05, board.size(), cv::noArray(), cv::ITERATIVE);
-#elif CV_MAJOR_VERSION == 3
+#elif CV_MAJOR_VERSION >= 3
         cv::solvePnPRansac(board, points[index], cameraMatrix, distortion, rvec, translation, false, 300, 0.05, 0.99, cv::noArray(), cv::SOLVEPNP_ITERATIVE);
 #endif
         cv::Rodrigues(rvec, rotation);
